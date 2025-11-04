292203eadfae6676bf261d8e0a8e792174c718c4
marianacazares <marianacazaresguzmanluzac@gmail.com>
2025-11-03 20:40:14 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,64 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
+import { getFirestore,
+         collection,
+         doc,
+         setDoc,
+         getDoc,
+         query,
+         where,
+         getDocs,
+         addDoc
+      } from "firebase/firestore";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBd8j2j4yjQF1HDbMyJMUnB8g-2Bp0ELZU",
+authDomain: "pagina-ely.firebaseapp.com",
+projectId: "pagina-ely",
+storageBucket: "pagina-ely.firebasestorage.app",
+messagingSenderId: "787334096832",
+appId: "1:787334096832:web:eb4934fdcb564f44160a25",
+measurementId: "G-DXTSCT11Q6"
+      };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+//Referencia al formulario
+const botonEnviar = document.querySelector('#enviar');
+
+//Funciones Firebase
+botonEnviar.addEventListener('click', async(e) =>{
+  e.preventDefault();
+  
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+if (!name || !email || !message) {
+  alert('Por favor, completa todos los campos.');
+  return;
+}
+try{
+  await addDoc(collection(db, 'contactos'), {
+    nombre:name,
+    correo:email,
+    mensaje:message,
+    fecha:new Date()
+  });
+
+console.log("Mensaje enviado correctamente:");
+console.log({
+  nombre:name,
+  correo:email,
+  mensaje:message,
+});
+alert("Tu mensaje se envió correctamente.");
+botonEnviar.reset();
+}catch(error){
+  console.error("Error al enviar el mensaje:", error);
+alert("Error al enviar el mensaje. Por favor, inténtalo nuevamente.");
+}
+
+});
